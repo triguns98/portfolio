7db3f9f9caeba7c8d31504e517d4440987d611b7
mihairobert <mihai.robert60@yahoo.com>
2024-09-23 23:06:25 +0300

    Second commit

--- src/Counter.js
+++ src/Counter.js
@@ -3,6 +3,8 @@ import { useState } from 'react';
 function Counter({name, btnName}) {
   const [count, setCount] = useState(0);
 
+  
+
     return (
         <div>
             <h4>{`${name}: ${count}`}</h4>
